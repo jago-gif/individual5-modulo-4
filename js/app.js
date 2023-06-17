69ba8aea973cbe73b584bbb5eebefa4082d00797
Your Name <[halcon1411@gmail.com]>
2023-06-16 20:31:09 -0400

    commit final

--- js/app.js
+++ js/app.js
@@ -1,3 +1,16 @@
+let comida;
+let opiniones = [];
+let meals = [];
+let tecla =0;
+let platosPasivos = [];
+   const mealContainer = document.getElementById("resultado");
+
+
+  const modal = document.getElementById("modalOpinion");
+
+  const instanciaModal = new bootstrap.Modal(modal);
+
+
 async function fetchMealCategories() {
   const apiUrl = "https://www.themealdb.com/api/json/v1/1/categories.php";
   const selectElement = document.getElementById("categorias");
@@ -8,7 +21,7 @@ async function fetchMealCategories() {
 
     const categories = strCategory(data.categories);
     console.log("Las categorias son: ");
-        console.log(categories);
+    console.log(categories);
 
     categories.forEach((category) => {
       const optionElement = document.createElement("option");
@@ -21,42 +34,50 @@ async function fetchMealCategories() {
   }
 }
 
-async function fetchMealsByCategory() {
-  const categorySelect = document.getElementById("categorias");
-  const selectedCategory = categorySelect.value;
-  const mealContainer = document.getElementById("resultado");
+async function fetchMealsByCategory(x) {
+   const categorySelect = document.getElementById("categorias");
+   const selectedCategory = categorySelect.value;
 
-  if (selectedCategory) {
-    const apiUrl = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${selectedCategory}`;
+   if (selectedCategory) {
+     const apiUrl = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${selectedCategory}`;
 
-    try {
-      const response = await fetch(apiUrl);
-      const data = await response.json();
-      console.log("Los platillos de la categoria seleccionada son ");
-      console.log(data);
+     try {
+       const response = await fetch(apiUrl);
+       const data = await response.json();
+       console.log("Los platillos de la categoria seleccionada son ");
+       console.log(data);
 
-      // Limpiar el contenedor de comidas antes de agregar las nuevas tarjetas
+       // Limpiar el contenedor de comidas antes de agregar las nuevas tarjetas
+       mealContainer.innerHTML = "";
+
+        meals = data.meals;
+        platosPasivos = meals;
+      cargarMeals();
+        
+     } catch (error) {
+       console.log("Error al obtener los datos:", error);
+     }
+   }
+}
+
+function cargarMeals(){
       mealContainer.innerHTML = "";
 
-      const meals = data.meals;
-      meals.forEach((meal) => {
-        const mealCard = createMealCard(meal);
-        mealContainer.appendChild(mealCard);
-      });
-    } catch (error) {
-      console.log("Error al obtener los datos:", error);
-    }
-  }
+  meals.forEach((meal) => {
+    const mealCard = createMealCard(meal);
+
+    mealContainer.appendChild(mealCard);
+  });
 }
 
 function createMealCard(meal) {
   const { idMeal, strMeal, strMealThumb } = meal;
 
   // Crear elementos HTML para la tarjeta
-  const card = document.createElement('div');
-  card.classList.add('card', 'col-md-4');
+  const card = document.createElement("div");
+  card.classList.add("card", "col-md-4");
 
-  const image = document.createElement('img');
+  const image = document.createElement("img");
   image.src = strMealThumb;
   image.alt = strMeal;
   image.classList.add("card-img-top");
@@ -65,6 +86,17 @@ function createMealCard(meal) {
   const cardBody = document.createElement("div");
   cardBody.classList.add("card-body");
   card.appendChild(cardBody);
+
+  const btnOpinion = document.createElement("button");
+  btnOpinion.classList.add("btn", "btn-primary");
+  btnOpinion.textContent = "Agregar opinion";
+  cardBody.appendChild(btnOpinion);
+
+ 
+  btnOpinion.onclick = function () {
+
+    abrirModal(strMeal);
+  }
 
   // Crear un elemento de título con la clase 'card-title' de Bootstrap
   const title = document.createElement("h3");
@@ -98,4 +130,91 @@ function strCategory(categories) {
 }
 
 fetchMealCategories();
+
+
+function abrirModal(meal) {
+ 
+  comida = meal
+  instanciaModal.show();
+  
+} 
+
+const guardarOpinion = document.getElementById("guardarOpinion");
+
+guardarOpinion.addEventListener("click", function () {
+
+const opinion = document.getElementById("opinion").value;
+const formularioOpinion = document.getElementById("formularioOpinion");
+
+let opinionInsert ={
+  comida: comida,
+  opinion: opinion
+} 
+
+opiniones.push(opinionInsert);
+ agregarLaOpinion()
+
+ instanciaModal.hide();
+
+formularioOpinion.reset();
+
+})
+
+function agregarLaOpinion(){
+  const opinionContainer = document.getElementById("lasOpiniones");
+  opinionContainer.innerHTML = "";
+
+  opiniones.forEach((opinion) => {
+    const card = document.createElement("div");
+    card.classList.add("card", "m-1");
+    card.style.width = "18rem";
+
+   
+
+    const cardBody = document.createElement("div");
+    cardBody.classList.add("card-body");
+
+    const title = document.createElement("h5");
+    title.classList.add("card-title");
+    title.textContent = opinion.comida;
+
+    const description = document.createElement("p");
+    description.classList.add("card-text");
+    description.innerHTML ="Opinion: <br>"+opinion.opinion;
+
+    cardBody.appendChild(title);
+    cardBody.appendChild(description);
+
+    card.appendChild(cardBody);
+    opinionContainer.appendChild(card);
+
+  });
+}
+
+document.getElementById("filtroTexto").addEventListener("keyup", function () {
+  let x = document.getElementById("filtroTexto").value.toLowerCase();
+  if(x.length==(tecla-1)){
+    fetchMealsByCategory(x);
+    return cargarComidas(x);
+  }
+  tecla = x.length;
+  cargarComidas(x);
+});
+
+function cargarComidas(x) {
+  const comidas = platosPasivos.filter((meal) =>
+    meal.strMeal.toLowerCase().includes(x)
+  );
+
+  mealContainer.innerHTML = "";
+
+  comidas.forEach((meal) => {
+    const mealCard = createMealCard(meal);
+    mealContainer.appendChild(mealCard);
+  });
+
+  meals.length = 0;
+  Array.prototype.push.apply(meals, comidas);
+}
+
 
